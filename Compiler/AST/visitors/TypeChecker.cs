e4ebe128a4d4233d8cbc84f7e306a3888a37df35
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:17:58 +0200

    Update TypeChecker.cs

--- Compiler/AST/visitors/TypeChecker.cs
+++ Compiler/AST/visitors/TypeChecker.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Compiler.AST.nodes;
 
 namespace Compiler.AST.visitors
 {
     class TypeChecker : IASTVisitor
     {
+        public object Visit(plus plus)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
