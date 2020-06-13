b90eaa7963fe02debf4d7dc16ae6088a241bbfc5
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 22:28:57 +0200

    Create TokenType.cs

---
+++ Compiler/token/TokenType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Compiler.token
+{
+    public enum TokenType
+    {
+        [RegularExpression("^ ")]
+        WHITE_SPACE,
+
+        [RegularExpression("^\\,)")]
+        COMMA,
+
+        [RegularExpression("^\\(")]
+        LPAR,
+
+        [RegularExpression("^\\)")]
+        RPAR,
+
+        [RegularExpression("^\\[")]
+        LBRA,
+
+        [RegularExpression("^\\]")]
+        RBAR,
+
+        [RegularExpression("^\\{")]
+        LCUR,
+
+        [RegularExpression("^\\}")]
+        RCUR,
+
+        [RegularExpression("^\\+)")]
+        PLUS,
+
+        [RegularExpression("^\\-)")]
+        MINUS,
+
+        [RegularExpression("^\\*)")]
+        MULTIPLY,
+
+        [RegularExpression("^\\/)")]
+        DIVIDE,
+
+        [RegularExpression("^\\%)")]
+        MODULO,
+    }
+}
