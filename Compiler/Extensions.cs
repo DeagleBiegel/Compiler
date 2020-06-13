6a81f77e34a75815181b1171893982a483a3722e
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 22:54:06 +0200

    Update Extensions.cs

--- Compiler/Extensions.cs
+++ Compiler/Extensions.cs
@@ -7,6 +7,7 @@ public static class Extensions
     /// <summary>
     ///     A generic extension method that aids in reflecting 
     ///     and retrieving any attribute that is applied to an `Enum`.
+    ///     https://stackoverflow.com/questions/13099834/how-to-get-the-display-name-attribute-of-an-enum-member-via-mvc-razor-code
     /// </summary>
     public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
             where TAttribute : Attribute
