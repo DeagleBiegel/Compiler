4e7fa834050c36eec2cd19587a5381d8c73fec62
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:17:00 +0200

    Create CodeGenerator.cs

---
+++ Compiler/AST/visitors/CodeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.AST.nodes;
+
+namespace Compiler.AST.visitors
+{
+    class CodeGenerator : IASTVisitor
+    {
+        public object Visit(plus plus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
