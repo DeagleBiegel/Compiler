3a20ddede1d900dc1ce28c7a215189caeca1c899
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:16:58 +0200

    Create plus.cs

---
+++ Compiler/AST/nodes/plus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.AST.nodes
+{
+    public class plus : ASTNode
+    {
+        public override object Accept(IASTVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
