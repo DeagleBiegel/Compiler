b7d82542226b09771cf63be39affbca355bfe2c8
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:17:04 +0200

    Create TokenStream.cs

---
+++ Compiler/token/TokenStream.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.token
+{
+    class TokenStream
+    {
+        private List<Token> tokens;
+
+        private int index;
+
+        public TokenStream(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            index = 0;
+        }
+
+        public bool Peek(TokenType type)
+        {
+            return tokens[index].Equals(type);
+        }
+    }
+}
