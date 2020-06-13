be03892695965edebf4abcd856b138a7191ee2c6
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:16:52 +0200

    Create ASTNode.cs

---
+++ Compiler/AST/ASTNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.AST
+{
+    public abstract class ASTNode
+    {
+        public abstract object Accept(IASTVisitor visitor);
+    }
+}
