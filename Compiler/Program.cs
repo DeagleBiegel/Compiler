2f7832d11484b269bbf45a06bef4727d7646fe6b
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 22:50:44 +0200

    Update Program.cs

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Compiler.token;
+using System;
 
 namespace Compiler
 {
@@ -6,7 +7,12 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tokenizer tokenizer = new Tokenizer("/* test */ ( + ) *");
+
+            foreach (Token token in tokenizer.FilteredTokens())
+            {
+                Console.WriteLine("{0} : {1}", token.Type, token.Value);
+            }
         }
     }
 }
