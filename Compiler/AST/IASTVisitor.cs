7ec7f3819c3b76a0e0d762a77f28113c9c6ebe96
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-06-13 23:16:57 +0200

    Create IASTVisitor.cs

---
+++ Compiler/AST/IASTVisitor.cs
@@ -0,0 +1,12 @@
+﻿using Compiler.AST.nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.AST
+{
+    public interface IASTVisitor
+    {
+        public object Visit(plus plus);
+    }
+}
